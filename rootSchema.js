0cd563368ed448f22b2e6cb73e57d884620cecbc
jacobworrel <jacobworrel@gmail.com>
2019-11-22 00:04:44 -0800

    add support for nested schemas

--- rootSchema.js
+++ rootSchema.js
@@ -4,8 +4,14 @@ module.exports = joi.object().keys({
   version: joi.number().meta({ default: '1.0.0' }).description('Follows semantic versioning.'),
   group: joi.number().required(),
   type: joi.string().required(),
-  o: joi.object(),
+  childSchema: joi.object().keys({
+    foo: joi.string(),
+    bar: joi.string(),
+  }).meta({
+    name: 'Child Schema',
+    filename: 'childSchema',
+  }),
 }).meta({
-  name: 'Test Schema',
-  filename: 'testSchema',
+  name: 'Root Schema',
+  filename: 'rootSchema',
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const json2md = require('json2md');
 const R = require('ramda');
-const testSchema = require('./../testSchema');
+const rootSchema = require('./../rootSchema');
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
@@ -10,7 +10,6 @@ const writeFileAsync = promisify(fs.writeFile);
 
 const primitiveList = ['string', 'number', 'boolean'];
 
-const schema = testSchema.describe();
 const findMeta = x => R.pipe(
   R.prop('metas'),
   R.defaultTo([]),
@@ -18,59 +17,106 @@ const findMeta = x => R.pipe(
   R.defaultTo({}),
   R.prop(x)
 );
-const name = findMeta('name')(schema);
-const filename = findMeta('filename')(schema);
 
 const makeSchemaTitle = R.objOf('h1');
 const makeKeyTitle = R.objOf('h2');
-const makeBlockquote = R.objOf('blockquote');
+const makeDescription = R.objOf('p');
 const wrapInBackticks = x => `\`${x}\``;
 
-const primitives = R.pipe(
-  R.prop('keys'),
-  R.filter(
-    R.pipe(
-      R.prop('type'),
-      R.includes(R.__, primitiveList)
+const makeTypeDef = R.pipe(
+  R.reject(R.either(R.isEmpty, R.isNil)),
+  R.join(' | '),
+  R.objOf('blockquote')
+);
+const makeType = x => wrapInBackticks(x);
+const makeRequiredOrOptional = isRequired => `${isRequired ? 'Required' : 'Optional'}`;
+const makeDefault = R.when(
+  R.complement(R.isNil),
+  R.pipe(
+    wrapInBackticks,
+    R.concat('Default: ')
+  )
+);
+const makeParentFilenameLink = parentFilename => `[Back to Parent Schema](./${parentFilename}.md)`;
+
+const isPrimitive = R.pipe(
+  R.prop('type'),
+  R.includes(R.__, primitiveList)
+);
+
+// PRIMITIVE TYPES
+const makePrimitiveTitle = x => makeKeyTitle(x);
+const makePrimitiveField = (val, key) => {
+  const {
+    type,
+    flags: {
+      presence = 'optional',
+      description = '',
+    } = {},
+  } = val;
+  const isRequired = R.equals(presence, 'required');
+  const defaultVal = findMeta('default')(val);
+  return [
+    makePrimitiveTitle(key),
+    makeTypeDef([
+      makeType(type),
+      makeRequiredOrOptional(isRequired),
+      makeDefault(defaultVal),
+    ]),
+    makeDescription(description),
+  ]
+};
+
+// OBJECT TYPES
+const makeObjectTitle = R.pipe(
+  key => `[${key}](./${key}.md)`,
+  makeKeyTitle
+);
+const makeObjectField = (val, key) => {
+  const {
+    type,
+    flags: {
+      presence = 'optional',
+      description = '',
+    } = {},
+  } = val;
+  const isRequired = R.equals(presence, 'required');
+  return [
+    makeObjectTitle(key),
+    makeTypeDef([
+      makeType(type),
+      makeRequiredOrOptional(isRequired),
+    ]),
+    makeDescription(description),
+  ]
+};
+
+function parseSchema (schema, parentFilename) {
+  const name = findMeta('name')(schema);
+  const filename = findMeta('filename')(schema);
+
+  const json = R.pipe(
+    R.prop('keys'),
+    R.mapObjIndexed((val, key) => {
+      if (isPrimitive(val)) {
+        return makePrimitiveField(val, key);
+      }
+      else {
+        parseSchema(val, filename);
+        return makeObjectField(val, key)
+      }
+    }),
+    R.values,
+    R.prepend(makeSchemaTitle(name)),
+    R.when(
+      () => R.not(R.isNil(parentFilename)),
+      R.prepend(makeParentFilenameLink(parentFilename))
     )
-  ),
-  R.mapObjIndexed((val, key) => {
-    const {
-      type,
-      flags: {
-        presence = 'optional',
-        description = '',
-      },
-    } = val;
-    const isRequired = R.equals(presence, 'required');
-    const defaultVal = findMeta('default')(val);
-    return [
-      makeKeyTitle(key),
-      makeBlockquote(
-        R.pipe(
-          R.reject(R.either(R.isEmpty, R.isNil)),
-          R.join(' | '),
-        )([
-          wrapInBackticks(type),
-          `${isRequired ? 'required' : 'optional'}`,
-          R.when(
-            R.complement(R.isNil),
-            R.pipe(
-              wrapInBackticks,
-              R.concat('defaults to ')
-            )
-          )(defaultVal),
-          description,
-        ])
-      )
-    ]
-  }),
-  R.values,
-)(schema);
+  )(schema);
 
-const md = json2md([
-  makeSchemaTitle(name),
-  ...primitives
-]);
+  const markdown = json2md(json);
+  writeFileAsync(pathTo(`./../dist/${filename}.md`), markdown, 'utf-8');
+}
 
-writeFileAsync(pathTo(`./../dist/${filename}.md`), md, 'utf-8');
+const root = rootSchema.describe();
+parseSchema(root);
