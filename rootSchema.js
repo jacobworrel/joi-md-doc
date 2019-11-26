a07b2bc6859a9cd50c0325af57c121c3ae6ba8ec
jacobworrel <jacobworrel@gmail.com>
2019-11-25 19:15:15 -0800

    clean up root schema

--- rootSchema.js
+++ rootSchema.js
@@ -4,7 +4,7 @@ const R = require('ramda');
 const listItem = joi
   .object()
   .keys({
-    quux: joi.string(),
+    foo: joi.string(),
   })
   .meta({
     name: 'List Item',
@@ -18,8 +18,7 @@ module.exports = joi
       .number()
       .meta({ default: '1.0.0' })
       .description('Follows semantic versioning.'),
-    group: joi.number().required(),
-    type: joi
+    foo: joi
       .string()
       .required()
       .valid(...R.map(R.pipe(R.toString, R.concat('valid')), R.range(0, 4)))
@@ -28,24 +27,23 @@ module.exports = joi
       ),
     primitiveList: joi.array().items(joi.string(), joi.number(), joi.boolean()),
     objectList: joi.array().items(listItem),
-    level1Schema: joi
+    nestedObjSchema1: joi
       .object()
       .keys({
         foo: joi.string(),
-        bar: joi.string(),
-        level2Schema: joi
+        nestedObjSchema2: joi
           .object()
           .keys({
-            baz: joi.string(),
+            foo: joi.string(),
           })
           .meta({
-            name: 'Level 2 Schema',
-            filename: 'level2Schema',
+            name: 'Nested Object Schema 2',
+            filename: 'nestedObjSchema2',
           }),
       })
       .meta({
-        name: 'Level 1 Schema',
-        filename: 'level1Schema',
+        name: 'Nested Object Schema 1',
+        filename: 'nestedObjSchema1',
       }),
   })
   .meta({
